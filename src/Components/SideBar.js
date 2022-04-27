ce9c3f38088c9c26d69d97566871b4f69b61537e
Muhammed Aslam <aslamccmc@gmail.com>
2022-04-27 05:54:13 +0530

    side bar created

---
+++ src/Components/SideBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../Components/SideBar.css"
+
+function SideBar() {
+  return (
+    <div className='side-bar'>
+
+    </div>
+  )
+}
+
+export default SideBar
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import NavBar from '../Components/NavBar'
+import SideBar from '../Components/SideBar'
 
 function Home() {
   return (
     <div>
         <NavBar/>
+        <SideBar/>
         
-        <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
-        <i class="fa-solid fa-magnifying-glass"></i>
+        {/* <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+        <i class="fa-solid fa-magnifying-glass"></i> */}
     </div>
   )
 }
